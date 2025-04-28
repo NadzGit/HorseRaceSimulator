a93c1309e35d7c8a7ed23accb45071d65a5d9e07
NadzGit <nadineseddat@gmail.com>
2025-04-28 12:47:34 +0100

    Cleaning up directories..

--- Part 2/StartRaceGUI.java
+++
@@ -1,7 +0,0 @@
-public class StartRaceGUI {
-    public static void main(String[] args) throws InterruptedException {
-        // Initialize and start the graphical race menu
-        MainMenuGUI mainMenuGUI = new MainMenuGUI();
-        mainMenuGUI.startRaceGUI();  // Invoke the method to start the graphical race simulation
-    }
-}
